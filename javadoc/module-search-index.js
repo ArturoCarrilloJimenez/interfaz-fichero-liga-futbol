6b3f31809c2a5c45b74748df1ad2ce52ca63494e
arturillo40 <arturojimenezalmontaras@gmail.com>
2024-03-14 14:08:15 +0100

    javadoc

---
+++ javadoc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"jv.interfazligafutbol"}];updateSearchResults();
---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"jv.interfazligafutbol","l":"jv.interfazligafutbol"}];updateSearchResults();
